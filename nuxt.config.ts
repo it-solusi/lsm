be27ec038f02604bedd71b1b183cd03aedc6e78a
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2025-03-19 00:31:23 +0700

    setup eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
 
   devtools: {
     enabled: true,
   },
 
   modules: [
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-    "@nuxt/icon",
-    "@nuxt/image",
-    "@nuxt/scripts",
-    "@nuxt/test-utils",
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
   ],
-});
+})
