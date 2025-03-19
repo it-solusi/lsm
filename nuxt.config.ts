fc49e2508c4bfa3cb22b60130f79bf7497fa73aa
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2025-03-19 23:49:39 +0700

    change vscode settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: {
     enabled: true,
   },
-  
-  css: ['~/assets/css/tailwind.css'],
+
+  css: ["~/assets/css/tailwind.css"],
 
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    'shadcn-nuxt',
-    '@nuxtjs/tailwindcss',
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "shadcn-nuxt",
+    "@nuxtjs/tailwindcss",
   ],
-})
+});
